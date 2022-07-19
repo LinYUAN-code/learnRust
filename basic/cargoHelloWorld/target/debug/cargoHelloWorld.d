2febf3dbe8c85cce57f0d3809f6956f0ec95caf2
renjunlin <renjunlin@tencent.com>
2022-07-19 21:27:04 +0800

    init

---
+++ basic/cargoHelloWorld/target/debug/cargoHelloWorld.d
@@ -0,0 +1,1 @@
+C:\Users\renjunlin\Desktop\learnRust\basic\cargoHelloWorld\target\debug\cargoHelloWorld.exe: C:\Users\renjunlin\Desktop\learnRust\basic\cargoHelloWorld\src\main.rs
---
+++ basic/cargoHelloWorld/target/debug/deps/cargoHelloWorld.d
@@ -0,0 +1,5 @@
+C:\Users\renjunlin\Desktop\learnRust\basic\cargoHelloWorld\target\debug\deps\cargoHelloWorld.exe: src\main.rs
+
+C:\Users\renjunlin\Desktop\learnRust\basic\cargoHelloWorld\target\debug\deps\cargoHelloWorld.d: src\main.rs
+
+src\main.rs:
